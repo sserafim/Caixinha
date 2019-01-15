90bb7ed7ae9d83ddf36c3200321681ac2f26b7c2
sserafim <samuel.serafim2010@gmail.com>
2019-01-14 23:18:53 -0200

    add categories module and firebase2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyDTp1PFlFJlFSc8f3jgW3PKYyetQTNsCh8",
+      authDomain: "caixinha-a4dec.firebaseapp.com",
+      databaseURL: "https://caixinha-a4dec.firebaseio.com",
+      projectId: "caixinha-a4dec",
+      storageBucket: "caixinha-a4dec.appspot.com",
+      messagingSenderId: "464884459434"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDTp1PFlFJlFSc8f3jgW3PKYyetQTNsCh8",
+    authDomain: "caixinha-a4dec.firebaseapp.com",
+    databaseURL: "https://caixinha-a4dec.firebaseio.com",
+    projectId: "caixinha-a4dec",
+    storageBucket: "caixinha-a4dec.appspot.com",
+    messagingSenderId: "464884459434"
+  }
 };
 
 /*
